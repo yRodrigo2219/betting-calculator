f6a33034a97a1329a4c390f82b49908cb6833042
Rodrigo <rodrigo2219dg@gmail.com>
2020-06-24 11:53:54 -0300

    Updates

---
+++ precache-manifest.326919d5f653fad13d38097663194700.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178d8a5256692f2e7c1b6f449e970f2e",
+    "url": "/betting-calculator/index.html"
+  },
+  {
+    "revision": "2ef0e51ba2516620baf4",
+    "url": "/betting-calculator/static/css/main.7145b233.chunk.css"
+  },
+  {
+    "revision": "88dde748b1602acafac3",
+    "url": "/betting-calculator/static/js/2.22b58ace.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/betting-calculator/static/js/2.22b58ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef0e51ba2516620baf4",
+    "url": "/betting-calculator/static/js/main.37ba89a2.chunk.js"
+  },
+  {
+    "revision": "a226849d32c50d40f958",
+    "url": "/betting-calculator/static/js/runtime-main.b511c157.js"
+  },
+  {
+    "revision": "789230e2015de47a01aa0990df1fa2c9",
+    "url": "/betting-calculator/static/media/home.789230e2.svg"
+  },
+  {
+    "revision": "70d6ca538e0ecf3c880c968dfa1df24a",
+    "url": "/betting-calculator/static/media/odd.70d6ca53.svg"
+  },
+  {
+    "revision": "9eda9bdd884ab73f962327dca433f482",
+    "url": "/betting-calculator/static/media/percentage.9eda9bdd.svg"
+  },
+  {
+    "revision": "1f49401a7823af540cd1eb507a2ea60d",
+    "url": "/betting-calculator/static/media/wallet.1f49401a.svg"
+  },
+  {
+    "revision": "9bd2177d2461f47d18d074d9ba421d2a",
+    "url": "/betting-calculator/static/media/whistle.9bd2177d.svg"
+  }
+]);
